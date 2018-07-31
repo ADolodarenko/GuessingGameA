5e15f0e8c094e662ce1f4977bc1e361e17b5e48f
ADolodarenko <adolodar@gmail.com>
2018-07-31 19:51:15 +0300

    Proceeding with the Guessing game for Android

--- app/src/main/java/org/dav/android/guessinggame/MainActivity.java
+++ app/src/main/java/org/dav/android/guessinggame/MainActivity.java
@@ -5,18 +5,78 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int TRYING_LIMIT = 7;
 
     private EditText txtGuess;
     private Button btnGuess;
     private TextView lblOutput;
+    private int theNumber;
+    private int tries;
+
+    public void checkGuess()
+    {
+        String guessText = txtGuess.getText().toString();
+        String message = "";
+
+        try
+        {
+            int guess = Integer.parseInt(guessText);
+
+            tries--;
+
+            if (guess == theNumber)
+            {
+                message = guess + " is correct. You win after " + (TRYING_LIMIT - tries) + " tries! Let's play again!";
+
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                newGame();
+            }
+            else
+            {
+                if (guess < theNumber)
+                    message = guess + " is too low.";
+                else
+                    message = guess + " is too high.";
+
+                if (tries > 0)
+                    message = message + " Try again.";
+                else
+                {
+                    message = message + " You lost. The number was " + theNumber + ". Let's play again!";
+
+                    Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
+                    newGame();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            message = "Enter a whole number between 1 and 100.";
+        }
+        finally
+        {
+            lblOutput.setText(message);
+            txtGuess.requestFocus();
+            txtGuess.selectAll();
+        }
+    }
+
+    public void newGame()
+    {
+        theNumber = (int)(Math.random() * 100 + 1);
+        tries = TRYING_LIMIT;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +86,28 @@ public class MainActivity extends AppCompatActivity {
         txtGuess = (EditText)findViewById(R.id.txtGuess);
         btnGuess = (Button)findViewById(R.id.btnGuess);
         lblOutput = (TextView)findViewById(R.id.lblOutput);
+
+        newGame();
+
+        btnGuess.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                checkGuess();
+            }
+        });
+
+        /*txtGuess.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent)
+            {
+                checkGuess();
+
+                return true;
+            }
+        });*/
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
